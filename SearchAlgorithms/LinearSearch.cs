1dfac3cdd28c3b21b9f7e4bd2421595f9daf7756
akinlami <bi54jf@student.sunderland.ac.uk>
2024-09-26 13:06:26 +0100

    Add project files.

---
+++ SearchAlgorithms/LinearSearch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithms
+{
+    internal class LinearSearch
+    {
+        public static int  Perform (double searchValue, List<double> listToSearch) 
+        {
+            for (int i = 0; i < listToSearch.Count; i++)
+            {
+
+                if (listToSearch[i] == searchValue)
+
+                    return i;
+            }
+        
+        
+        }
+}
